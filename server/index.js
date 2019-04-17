b07e706228342aae428ce7e41304096dafe2d02f
Benjamin Van Treese <vantreeseba@gmail.com>
2019-04-17 17:01:47 -0500

    Make serve built react app

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const express = require('express')
+const express = require('express');
 const path = require('path');
-const app = express()
+const app = express();
 const port = process.env.PORT;
 
 
-app.use(express.static(path.resolve(__dirname, './static/')))
-app.get('/api', (req, res) => res.send('Hello World!'))
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+app.get('/api', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
